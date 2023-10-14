a27405c98ed6c190545415865e13893063079f17
Atul <atul_kumar@boffincoders.com>
2023-10-14 14:30:59 +0530

    Changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import pdfRouter from "./routes";
 const app = express();
-const port = process.env.GPDFTASK_PORT || 9000;
+const port = process.env.GPDFTASK_PORT || 9001;
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
